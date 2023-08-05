83958d38e2c47321c50a0320c219ab0d4020614c
athanasiia <grvkan@gmail.com>
2023-08-05 12:23:35 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  n += '';
+  let res = '';
+  for (let i = n.length - 1; i >= 0; i--) {
+    if (n[i] === '-') continue;
+    res += n[i];
+  }
+  return res * 1;
 }
